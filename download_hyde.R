218c1a34c6b059e0dfbfb0c30aee6aafe181dd03
Nick Gauthier <ngauthier@email.arizona.edu>
2020-07-29 18:30:31 -0700

    more efficient hyde download

--- download_hyde.R
+++ download_hyde.R
@@ -1,17 +1,21 @@
 library(RCurl)
 library(dplyr)
 library(purrr)
+library(stringr)
 
 url_hyde <- 'ftp://ftp.pbl.nl/hyde/hyde3.2/baseline/zip/'
 url_general <- 'ftp://ftp.pbl.nl/hyde/hyde3.2/general_files/'
 
-destination <- '~/Downloads/HYDE/'
+destination <- '~/Downloads/HYDE/hyde32_output/'
 
-getURL(url_hyde,dirlistonly = TRUE) %>%
+hyde_names <- str_c('cropland', 'grazing', 'ir_rice', 'popc', 'tot_irri', 'uopp', sep = '|')
+recent_years <- str_c(paste0(2001:2011, 'AD'), collapse = '|')
+
+getURL(url_hyde, dirlistonly = TRUE) %>%
   strsplit('\n') %>%
   unlist() %>%
-  .[55:150] %>%
-  walk(~download.file(paste0(url_hyde, .), paste0(destination, .)))
+  str_subset(recent_years, negate = TRUE) %>%
+  walk(~get_hyde(paste0(url_hyde, .), paste0(destination, .)))
 
 getURL(url_general, dirlistonly = TRUE) %>%
   strsplit('\n') %>%
@@ -25,14 +29,20 @@ hyde_file <- paste0('cropland', time_steps, '.asc')
 
 # if not present in raw-data directory, download land use zip files
 # and extract only the cropland file to the raw-data directory
-get_hyde <- function(url, file){
-  if(!file.exists(paste0('data/raw-data/HYDE/', file))){
+get_hyde <- function(url, destination){
+#  if(!file.exists(paste0(destination, file))){
     temp <- tempfile()
     download.file(url, temp)
-    unzip(temp, files = file, exdir = 'data/raw-data/HYDE',
+
+    # get a list of the files and find only the variables we care about
+    files <- unzip(temp, list = TRUE)$Name %>%
+      str_subset(., hyde_names)
+
+    unzip(temp, files = files, exdir = destination,
           overwrite = TRUE)
+
     unlink(temp)
-  }
+ # }
 }
 
 # run get_hyde function on the lists of files above
