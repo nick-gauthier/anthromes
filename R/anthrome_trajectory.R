24fa4bc816c5abc8a86b07c14fa3658676a6d74c
Nick Gauthier <ngauthier@email.arizona.edu>
2020-10-06 14:41:56 -0700

    add function to compute anthrome trajectories automatically

---
+++ R/anthrome_trajectory.R
@@ -0,0 +1,24 @@
+#' Calculate Anthrome trajectories
+#'
+#' @param data
+#' @param by
+#'
+#' @return
+#' @export
+#'
+#' @examples
+anthrome_trajectory <- function(data, by = NULL) {
+  data %>%
+    left_join(anthrome_key, by = 'anthrome') %>% # join before counting so empty factor levels are preserved
+    count(time_step, class, {{ by }}, wt = land_area, name = 'land_area', .drop = FALSE) %>%
+    left_join(time_key, by = 'time_step') %>%
+    mutate(period = case_when(year %in% c(seq(-10000, -1000, 1000), 1) ~ 'Millennia',
+                              year %in% seq(100, 1700, 100) ~ 'Centuries',
+                              year %in%  c(seq(1710, 2010, 10), 2017) ~ 'Decades'),
+           period = factor(period, c('Millennia', 'Centuries', 'Decades'))) %>%
+    group_by(time_step, {{ by }}) %>%
+    mutate(percent_land_area = land_area / sum(land_area)) %>%
+    ungroup() %>%
+    arrange(class, {{ by }}, year) %>%
+    select(class, {{ by }}, time_step, percent_land_area, period, year)
+}
