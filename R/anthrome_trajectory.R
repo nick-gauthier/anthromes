987b79b9d33ced09ddac9a53cfcf6d2ae2d14d32
Nick Gauthier <ngauthier91@gmail.com>
2021-01-27 19:02:43 -0700

    reorganize hyde population and anthrome legacy functions

--- R/anthrome_trajectory.R
+++ R/anthrome_trajectory.R
@@ -23,3 +23,9 @@ anthrome_trajectory <- function(data, by = NULL) {
     arrange(class, {{ by }}, year) %>%
     select(class, {{ by }}, time_step, percent_land_area, period, year)
 }
+
+read_anthromes <- function(path) {
+  read_csv(path, col_types = cols(.default = 'i')) %>%
+    select(-c(`2001AD`:`2009AD`, `2011AD`:`2016AD`)) %>% # remove annual data
+    pivot_longer(-id, names_to = 'time_step', values_to = 'anthrome')
+}
--- R/hyde_utils.R
+++ R/hyde_utils.R
@@ -1,4 +1,3 @@
-
 #' Title
 #'
 #' @param hyde_varname
@@ -23,4 +22,20 @@ hyde2dgg <- function(hyde_varname, dgg, hyde_path){
 
   setNames(hyde, time_steps) %>%
     mutate(ANL12_ID = dgg$ANL12_ID, .before = 1)
+}
+
+
+get_hyde_pop <- function(hyde, by = NULL) {
+  hyde_pop %>%
+    left_join(an12_dgg_inputs, by = c('ANL12_ID' = 'id')) %>%
+    group_by({{ by }}, time_step) %>%
+    summarise(population = sum(population)) %>%
+    mutate(pop_percent = population / max(population)) %>%
+    ungroup() %>%
+    left_join(time_key, by = 'time_step') %>%
+    filter(!(year %in% c(2001:2009,2011:2016))) %>%
+    mutate(period = case_when(year %in% c(seq(-10000, -1000, 1000), 1) ~ 'Millennia',
+                              year %in% seq(100, 1700, 100) ~ 'Centuries',
+                              year %in% c(seq(1710, 2010, 10), 2017) ~ 'Decades'),
+           period = factor(period, c('Millennia', 'Centuries', 'Decades')))
 }
