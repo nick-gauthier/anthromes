f16892f22c5879c36f45ceb2470e2782b50a658e
Nick Gauthier <ngauthier@email.arizona.edu>
2021-03-04 19:31:32 -0700

    split preprocessing split into regridding and classification steps
    clean up regridding process and put it in its own script so the anthromes classifier can run with less overhead

--- R/hyde_utils.R
+++ R/hyde_utils.R
@@ -12,9 +12,9 @@ hyde2dgg <- function(hyde_varname, dgg, hyde_path){
     hyde <- list.files(hyde_path, recursive = TRUE, full.names = TRUE) %>% # does this catch the zip files too? better to unzip here?
       str_subset(hyde_varname) %>%
       map(raster) %>%
-      brick %>%
+      brick() %>%
       `crs<-`(value = '+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0') %>%
-      exact_extract(dgg, 'sum')
+      exact_extract(dgg, 'sum', progress = TRUE)
   )
   time_steps <- names(hyde) %>%
     str_sub(start = str_length(hyde_varname) + 5) %>%
@@ -27,7 +27,7 @@ hyde2dgg <- function(hyde_varname, dgg, hyde_path){
 #' @export
 get_hyde_pop <- function(hyde, by = NULL) {
   hyde_pop %>%
-    left_join(an12_dgg_inputs, by = c('ANL12_ID' = 'id')) %>%
+    left_join(an12_dgg_inputs, by = 'ANL12_ID') %>%
     group_by({{ by }}, time_step) %>%
     summarise(population = sum(population)) %>%
     mutate(pop_percent = population / max(population)) %>%
