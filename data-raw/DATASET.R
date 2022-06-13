e00e57ed64eaf928ca2ac4619c5e4285d8e4d4ac
Nick Gauthier <nicolas.gauthier@ufl.edu>
2022-06-13 15:39:34 -0400

    Update DATASET.R
    really just a commit to see if those Icon files were removed

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -9,9 +9,8 @@ bbox <- st_bbox(c(xmin = 29, xmax = 39, ymin = 30, ymax = 40), crs = 4326)
 hyde_med <- get_hyde(dir = 'data-raw/') %>%
   st_crop(bbox) %>%
   # current bug in stars loses offset info when filtered
-  #filter(time %in% time_steps_millennia()) %>%
-  # so subset instead
-  .[,,,c(8,9,10,11,21,58)] %>%
+  #filter(time %in% time_steps_millennia) %>%
+  .[,,,c(8,9,10,11,21,58)] %>%  # so subset instead
   st_as_stars() %>%
   # change the names to something more readable
   setNames(c('crops', 'grazing', 'rice', 'pop', 'irrigation', 'urban'))
@@ -23,4 +22,9 @@ dgg_med <- read_sf('dgg_land.gpkg')[st_as_sfc(bbox),, op = st_covered_by]
 
 anthromes_med <- get_anthromes(hyde_med, inputs_med)
 
-usethis::use_data(hyde_med, inputs_med, dgg_med, anthromes_med, overwrite = TRUE)
+usethis::use_data(hyde_med,
+                  inputs_med,
+                  dgg_med,
+                  anthromes_med,
+                  overwrite = TRUE)
+
